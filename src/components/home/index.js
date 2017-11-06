057cb77355956361b63176cdf75748a9a8a327cb
Michael Miller <mdm_8@hotmail.com>
2017-11-05 22:03:13 -0800

    Really should have been breaking out my work into branches. Did a bunch of troubleshooting with the react-bootstrap and react-router interactions. HAve basic routing set up with some placeholder components.

---
+++ src/components/home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div> Sample Text </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/song-stats/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SongStats extends React.Component {
+
+
+  render() {
+    return(
+      <div>This is the song stats page.</div>
+    );
+  }
+}
+
+export default SongStats;
